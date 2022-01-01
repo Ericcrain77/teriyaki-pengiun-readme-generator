e70bf1dd260437d4ee9cc14cf3fb3dc0aa38633d
Eric Crain <ericcrain77@gmail.com>
2022-01-01 18:05:25 -0500

    adjusting comments in generateMarkdown.js file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let licenseBadge = '';
@@ -19,7 +19,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   let licenseLink = '';
@@ -40,7 +40,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === 'none') {
@@ -55,7 +55,7 @@ function renderLicenseSection(license) {
   }
 }
 
-// Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.projectTitle}
